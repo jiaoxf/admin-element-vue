f61f71766b68249897d06c01612d2ed0c30ea630
焦学峰 <13261899915@163.com>
2020-10-29 18:04:40 +0800

    头部菜单与router同步

--- src/views/layout/components/Header.vue
+++ src/views/layout/components/Header.vue
@@ -34,25 +34,6 @@
           </el-submenu>
         </template>
       </el-menu>
-      <el-menu 
-      :default-active="activeIndex" 
-      class="el-menu-demo" 
-      mode="horizontal" 
-      @select="handleSelect" 
-      background-color="#fff" 
-      text-color="#3D80FF" 
-      active-text-color="#3D80FF"
-      >
-          <el-menu-item index="1">
-            <span>处理中心</span>
-          </el-menu-item>
-        <el-submenu index="2">
-          <template slot="title">我的工作台</template>
-          <el-menu-item index="2-1">选项1</el-menu-item>
-          <el-menu-item index="2-2">选项2</el-menu-item>
-          <el-menu-item index="2-3">选项3</el-menu-item>
-        </el-submenu>
-      </el-menu>
     </el-header>
   </div>
 </template>
